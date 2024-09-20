53ad8f19c4663393825d084aec94f4269aabae44
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-20 15:41:50 +0200

    feat: add guest index route & view

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.guest')
+
+@section('content')
+
+<div class="container my-5">
+
+    <h1 class="my-3">HOME</h1>
+    <h2 class="my-3">Pagina Pubblica</h2>
+    <p class="my-3">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, culpa dignissimos atque commodi est velit aut quasi animi iure cumque dolor fuga rerum asperiores, eaque officiis. Dolore, quis quo. Accusamus placeat blanditiis atque perferendis velit similique beatae nam reiciendis consectetur amet quia quae iste cumque eveniet sed voluptate, et eius esse. Quae voluptas odio fugit quam ducimus, accusantium, consectetur commodi maiores repellendus earum, alias sequi nesciunt minus omnis quidem. Debitis, quis blanditiis culpa placeat ut numquam pariatur ipsam illum atque animi rerum in praesentium alias, necessitatibus ipsa explicabo similique amet architecto ratione! Ducimus perferendis esse cupiditate nemo, possimus harum exercitationem.
+    </p>
+
+</div>
+
+
+@endsection
